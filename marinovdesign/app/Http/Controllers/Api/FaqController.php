2de655ef9b6708c8dac440e265f3ded0a3d1788f
Sasho Gjorgjiev <saso.gorgiev.11@gmail.com>
2024-02-17 13:52:58 +0100

    Created api route for faq

---
+++ marinovdesign/app/Http/Controllers/Api/FaqController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\Faq;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class FaqController extends Controller
+{
+    public function index()
+    {
+        $faqs = Faq::all();
+        return response()->json(['faqs' => $faqs]);
+    }
+}
